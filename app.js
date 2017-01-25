ee3630530f5c4227a3846c4422f35a129e719af3
Jufebrown <jufebrown@gmail.com>
2017-01-25 10:03:44 -0600

    review of onauthstatechange

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // example code
 
+firebase.auth().onAuthStateChange(function(user) {
+  if(user) {
+    //send to good page
+  } else {
+    //send to login page
+  }
+})
+
+
+
 .controller('MainCtrl', function ($q, $http) {
 
   if (firebase.auth().currentUser === null) {
